7a8d39c59e677967a517ca1a73a90c52fd0dd484
E. Almqvist <h1g.server@gmail.com>
2020-05-01 23:51:00 +0200

    Parameter stuff

--- src/steamget.cpp
+++ src/steamget.cpp
@@ -5,27 +5,40 @@ using namespace std;
 namespace steamget {
 	const char * url = "https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v0001/";
 
-	char * addonID = "1059823362"; // vars for the addon to be downloaded
-	char * outPath = "./";
+	void info( string txt, bool linebreak = true ) {
+		cout << txt;
+		if( linebreak ) {
+			cout << endl;
+		}
+	}
+	
+	struct params {
+		char * addonID = "NONE";
+		char * outPath = "./";
+	};
 
-	void getArgs( int argc, char ** argv ) {
+	params getArgs( int argc, char ** argv ) {
+		params returnParams;
 		for( int i = 0; i < argc; i++ ) {
-			if( argv[i][0] == '-' && sizeof(argv[i]) == 2 ) {
+			if( argv[i][0] == '-' ) {
 				if( argv[i][1] == 'i' ) { 
-					addonID = argv[i+1]; 
+					returnParams.addonID = argv[i+1]; 
 				} else if( argv[i][1] == 'o' ) {
-					outPath = argv[i+1];		
+					returnParams.outPath = argv[i+1];
 				}
 			}
 		}
+		return returnParams;
 	}
 
-	void downloadItem() {
-		PublishedFileId_t a;
+	void downloadItem( params addonInfo ) {
+		info( "Downloading addon: ", false );
+		cout << addonInfo.addonID << endl;
 	}
 }
 
 int main( int argc, char** argv ) {
-	steamget::getArgs( argc, argv ); // apply the arguments
+	steamget::params P = steamget::getArgs( argc, argv ); // apply the arguments
+	steamget::downloadItem( P );
 	return 0;
 }
